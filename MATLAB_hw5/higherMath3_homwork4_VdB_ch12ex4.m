fccb5c9211606a46378967c42d6fe896d4ec10e0
Stefan Stark <stark_st@gmx.net>
2016-11-29 12:52:45 +0100

    Renaming ex2 - 12.4 instead of 14.2

--- MATLAB_hw5/higherMath3_homwork4_VdB_ch12ex4.m
+++ MATLAB_hw5/higherMath3_homwork4_VdB_ch12ex4.m
@@ -1,8 +1,12 @@
+% Author: Stefan Stark
+% Date:   28.11.2016
+
 %% clean up
 clear all, close all, clc;
 
-% VdB Excercise 14.2 (page 212)
-% A hybrid of the bisection method and Newton’s method
+% VdB Excercise 12.4 (page 181)
+% A hybrid of the bisection method and Newtons method
+
 [x,xk, fxk] = myfzero('fct1',-1,0.9);
 figure; plot(abs(fxk),'o'); title('fxk');
 figure; plot(abs(xk-x),'o'); title('abs(xk-x)');
